f9964a7c2a06d43cf436b1c378d1d56932910d02
Joe Marzullo <jmarzu13@gmail.com>
2016-08-25 11:34:53 -0700

    More tests completed

--- casperTest/casperTest.js
+++ casperTest/casperTest.js
@@ -1,4 +1,4 @@
-casper.test.begin('Testing Good Reads', 5, function(test) {
+casper.test.begin('Testing Good Reads', 8, function(test) {
 
   casper.start('https://www.goodreads.com/', function() {
     test.assertTitle('Share Book Recommendations With Your Friends, Join Book Clubs, Answer Trivia', 'good reads has correct title');
@@ -15,6 +15,41 @@ casper.test.begin('Testing Good Reads', 5, function(test) {
             return __utils__.findAll("a.bookTitle").length >= 10;
         }, "good reads search for \"1984\" retrieves 10 or more results");
   });
+
+  casper.wait(3000, function() {
+    this.capture('casperTest/bookResults.png');
+    console.log('book search results successful');
+  });
+
+  casper.thenOpen('https://www.goodreads.com/', function() {
+    test.assertTitle('Share Book Recommendations With Your Friends, Join Book Clubs, Answer Trivia', 'navigated to home page');
+  });
+
+  casper.wait(3000, function() {
+    this.capture('casperTest/homePage.png');
+    console.log('back to the home page');
+  });
+
+  casper.then(function() {
+    test.assertExists('form[id="userSignupForm"]', 'sign up form exists');
+    this.fill('form[id="userSignupForm"]', {
+      'user[first_name]' : 'Joe',
+      'user[email]' : 'joe@boom.com',
+      'user[password]' : 'password'
+    });
+  });
+
+  casper.wait(3000, function() {
+    casper.capture('casperTest/enterInfo.png');
+    console.log('entered in user information');
+  });
+
+  casper.then(function() {
+    test.assertExists('input[value="Sign up"]', 'sign up button exists');
+    this.click('input[value="Sign up"]')
+  });
+
+  // test.assertUrlMatch(/contact/, 'New location is ' + this.getCurrentUrl());
 
   casper.wait(3000,function() {
     casper.capture('casperTest/grBookResults.png');
